6184cceff28f7d436df4ca7b126d87e0bc78e940
fff <assa.x0633.su@mail.ru>
2023-02-26 16:07:02 +0300

    upd

--- ScreenRecognition.Api/Data/ScreenRecognitionContext.cs
+++ ScreenRecognition.Api/Data/ScreenRecognitionContext.cs
@@ -31,7 +31,7 @@ public partial class ScreenRecognitionContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-BB20U5H;Database=ScreenRecognition;Trusted_Connection=True;User=fff;Password=05062003;TrustServerCertificate=True")
+        => optionsBuilder.UseSqlServer("Server=fff\\SQLEXPRESS;Database=ScreenRecognition;Trusted_Connection=True;User=fff;Password=05062003;TrustServerCertificate=True")
         .UseLazyLoadingProxies();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
