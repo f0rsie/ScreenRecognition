0624d2768dc5618f22080093855bd0e3a5e00d21
fff <assa.x0633.su@mail.ru>
2023-04-11 16:52:52 +0300

    code refactoring; deleted deprecated methods

--- ScreenRecognition.Api/Core/Services/ImageTransformationsService.cs
+++ ScreenRecognition.Api/Core/Services/ImageTransformationsService.cs
@@ -1,5 +1,4 @@
-﻿using ScreenRecognition.Api.Models;
-using ScreenRecognition.ImagePreparation.Services;
+﻿using ScreenRecognition.ImagePreparation.Services;
 using SkiaSharp;
 using System.Drawing;
 
@@ -11,119 +10,39 @@ namespace ScreenRecognition.Api.Core.Services
     public class ImageTransformationsService
     {
         private ImagePreparationService _imagePreparationService;
-
-        private List<Thread> _threads;
         private List<byte[]> _results;
-
-        private SKColor _startPixelColor;
-        private int _imageSize;
-
-        private int _countParts = 0;
-        private double _maxImagePartLength = 500000.0;
-
-        private List<ImageSeparationThreadModel> _newResults;
-
-        private byte[] _inputImage;
 
         public ImageTransformationsService()
         {
-            _threads = new List<Thread>();
-            _results = new List<byte[]>();
-
-            _newResults = new List<ImageSeparationThreadModel>();
-
-            _imagePreparationService = new ImagePreparationService();
+            _imagePreparationService = new();
+            _results = new();
         }
 
         // Работа с изображением
         public List<byte[]> PreparedImages(byte[] inputImage)
         {
-            List<byte[]>? result = new List<byte[]>();
-
-            try
+            _results = new()
             {
-                _results = new();
-                _threads = new();
-                _newResults = new();
+                Prepare(inputImage),
+            };
 
-                _inputImage = inputImage;
-                //var bmp = ImagePreparationService.ByteToBitmap(inputImage);
-                var bmp = SKBitmap.Decode(inputImage);
+            // Для дебага картинок
+            ByteToBitmap(inputImage).Save("D:/Original.png", System.Drawing.Imaging.ImageFormat.Png);
+            ByteToBitmap(_results.FirstOrDefault()).Save("D:/Result.png", System.Drawing.Imaging.ImageFormat.Png);
 
-                //_startPixelColor = bmp.GetPixel(new Random().Next(0, bmp.Width), new Random().Next(0, bmp.Height));
-                _startPixelColor = bmp.GetPixel(0, 0);
-                _imageSize = bmp.Width * bmp.Height;
-
-                List<ImageSeparationThreadModel> models = new();
-
-                int countParts = (int)(inputImage.Length / _maxImagePartLength);
-                if (countParts <= 0)
-                    countParts = 1;
-                else if (inputImage.Length / _maxImagePartLength > countParts)
-                    countParts++;
-
-                //var imageParts = ImageSeparation(bmp, countParts);
-                var imageParts = new List<byte[]> { inputImage };
-
-                int currentThreadsNumber = 0;
-
-                for (int i = 0; i < imageParts.Count; i++)
-                {
-                    var model = new ImageSeparationThreadModel(imageParts[i], i, "default");
-                    models.Add(model);
-                    _threads.Add(new Thread(Prepare));
-                    _threads[currentThreadsNumber].Start(model);
-
-                    currentThreadsNumber++;
-                }
-
-                while (true)
-                {
-                    if (_threads.Where(e => e.ThreadState == ThreadState.Running).Count() == 0)
-                        break;
-                }
-
-                //result = WholeImage(countParts);
-                result = _newResults.Select(e => e.ImagePart).ToList();
-
-                ByteToBitmap(inputImage).Save("D:/Original.png", System.Drawing.Imaging.ImageFormat.Png);
-                ByteToBitmap(result.FirstOrDefault()).Save("D:/Result.png", System.Drawing.Imaging.ImageFormat.Png);
-            }
-            catch { }
-
-            return result;
+            return _results;
         }
 
-        // На удаление в будущем
-        public static Bitmap ByteToBitmap(byte[] image)
+        // Метод обработки фото
+        private byte[] Prepare(byte[] startedImage)
         {
-            var result = new Bitmap(new MemoryStream(image));
-
-            return result;
+            return _imagePreparationService.GetPreparedImage(startedImage, SKColors.White, SKColors.Black);
         }
 
-        // New
-        private void Prepare(object? model)
+        // Для дебага картинок
+        private Bitmap ByteToBitmap(byte[] image)
         {
-            try
-            {
-                int i = 0;
-                var sepModel = model as ImageSeparationThreadModel;
-
-                var bmp = SKBitmap.Decode(sepModel?.ImagePart);
-
-                var image = _imagePreparationService.GetPreparedImage(bmp, SKColors.White, SKColors.Black, _startPixelColor, _imageSize);
-
-                ImageSeparationThreadModel result = new ImageSeparationThreadModel
-                {
-                    ImagePart = image,
-                    Number = sepModel.Number,
-                    Type = "default",
-                };
-
-                _newResults.Add(result);
-            }
-            catch { }
+            return new Bitmap(new MemoryStream(image));
         }
     }
 }
--- ScreenRecognition.ImagePreparation/Services/ImagePreparationService.cs
+++ ScreenRecognition.ImagePreparation/Services/ImagePreparationService.cs
@@ -4,18 +4,33 @@ namespace ScreenRecognition.ImagePreparation.Services
 {
     public class ImagePreparationService
     {
-        // New methods
-        public byte[] GetPreparedImage(SKBitmap image, SKColor firstColor, SKColor secondColor, SKColor startPixelColor, int imageSize)
+        private SKBitmap _image = null!;
+
+        private SKColor _startPixelColor;
+        private SKColor _firstColor;
+        private SKColor _secondColor;
+
+        private int _imageWidth = 0;
+        private int _imageHeight = 0;
+
+        public byte[] GetPreparedImage(byte[] image, SKColor firstColor, SKColor secondColor)
         {
-            var blackWhiteBitmap = ImageColorSetter(image, firstColor, secondColor, startPixelColor, imageSize);
+            _image = SKBitmap.Decode(image);
+            _firstColor = firstColor;
+            _secondColor = secondColor;
+            _startPixelColor = _image.GetPixel(0, 0);
+            _imageWidth = _image.Width;
+            _imageHeight = _image.Height;
+
+            var blackWhiteBitmap = ImageColorSetter();
             var result = blackWhiteBitmap.Encode(SKEncodedImageFormat.Png, 100).ToArray();
 
             return result;
         }
 
-        private SKBitmap ImageColorSetter(SKBitmap bmp, SKColor firstPixelColor, SKColor secondPixelColor, SKColor startPixelColor, int imageSize)
+        private SKBitmap ImageColorSetter()
         {
-            var result = bmp;
+            var result = _image;
 
             IntPtr pixelsPtr = result.GetPixels();
 
@@ -27,12 +42,12 @@ namespace ScreenRecognition.ImagePreparation.Services
                 {
                     for (int col = 0; col < result.Height; col++)
                     {
-                        var color = firstPixelColor;
+                        var color = _firstColor;
 
-                        if (IsColorsSimilar(startPixelColor, new SKColor(*ptr, *(ptr + 1), *(ptr + 2), *(ptr + 3)), imageSize))
-                            color = firstPixelColor;
+                        if (IsColorsSimilar(_startPixelColor, new SKColor(*ptr, *(ptr + 1), *(ptr + 2), *(ptr + 3))))
+                            color = _firstColor;
                         else
-                            color = secondPixelColor;
+                            color = _secondColor;
 
                         *ptr++ = color.Red;      // red
                         *ptr++ = color.Green;    // green
@@ -45,17 +60,12 @@ namespace ScreenRecognition.ImagePreparation.Services
             return result;
         }
 
-        private bool IsColorsSimilar(SKColor firstColor, SKColor secondColor, int imageSize)
+        private bool IsColorsSimilar(SKColor firstColor, SKColor secondColor)
         {
-            double ratio = 4;
-
-            if (imageSize >= 200000 && imageSize < 400000)
-                ratio = 2;
-            else if (imageSize >= 400000)
-                ratio = 1.5;
+            double ratio = Math.Abs(_imageWidth - _imageHeight);
 
             int colorDifference = (int)(255.0 / ratio);
-            colorDifference = 150;
+            colorDifference = 100;
 
             bool result = Math.Sqrt(Math.Pow(firstColor.Red - secondColor.Red, 2f) + Math.Pow(firstColor.Green - secondColor.Green, 2f) + Math.Pow(firstColor.Blue - secondColor.Blue, 2f)) < colorDifference;
 
